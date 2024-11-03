17d1037ac956a9194de95aacc9665cf567070453
dipexplorer <dipexplorerid23@gmail.com>
2024-11-03 08:24:47 +0530

    fixing err listing

--- admin.js
+++ admin.js
@@ -5,12 +5,6 @@ if(process.env.NODE_ENV!="production") { //not to deploy .env file while uploadi
 const mongoose = require('mongoose');
 const User = require("./models/user.js");
 
-// Check for environment variable
-if (process.env.SETUP_ADMIN !== "run-admin-setup") {
-    console.log("Unauthorized access to admin setup.");
-    process.exit(1);
-}
-
 const dbUrl = process.env.ATLAS_DB_TOKEN;
 
 async function createSingleAdminUser() {
@@ -21,22 +15,22 @@ async function createSingleAdminUser() {
         });
 
         // Check if the admin user already exists
-        const existingAdmin = await User.findOne({ username: process.env.ADMIN_USERNAME });
+        const existingAdmin = await User.findOne({ username: 'admin' });
         if (existingAdmin) {
             console.log("Deleting existing admin user...");
-            await User.deleteOne({ username: process.env.ADMIN_USERNAME }); // Delete the existing admin
+            await User.deleteOne({ username: 'admin' }); // Delete the existing admin
             console.log("Existing admin user deleted.");
         }
 
         // Create the new admin user
         const adminUser = new User({
-            username: process.env.ADMIN_USERNAME, // Admin username
-            email: process.env.ADMIN_EMAIL, // Admin email (make sure it's valid)
+            username: 'admin', // Admin username
+            email: 'admin@example.com', // Admin email (make sure it's valid)
             isAdmin: true // Set as admin
         });
 
         // Set the password
-        await User.register(adminUser, process.env.ADMIN_PASSWORD); // Register and hash the password
+        await User.register(adminUser, 'password'); // Register and hash the password
         console.log("New admin user created successfully!");
     } catch (error) {
         console.error("Error creating admin user:", error);
