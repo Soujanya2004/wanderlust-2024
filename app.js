788e204f3fe3a3519700b8fe18fec72d3afff7df
Suvadip-sana <suvadipsana602@gmail.com>
2024-11-09 13:54:33 +0530

    Fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV != "production") {
   require('dotenv').config();
 }
 
-const port = 8000;
+const port = 8080;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
